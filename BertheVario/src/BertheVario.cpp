47a0ba63259fe284c3efd8109ce73c88bc73ed29
SpinBerthe <spinberthe@gmail.com>
2024-08-07 23:45:42 +0200

    valeur par defaut config.txt

--- BertheVario/src/BertheVario.cpp
+++ BertheVario/src/BertheVario.cpp
@@ -4,10 +4,10 @@
 /// \brief Fichier principal du projet GNU-Vario de Berthe
 ///
 /// \date creation     : 02/03/2024
-/// \date modification : 28/07/2024
+/// \date modification : 07/08/2024
 ///
 
-char NumVer[] = "20240728c" ;
+char NumVer[] = "20240807a" ;
 
 // uncomment next line to use HSPI for EPD (and e.g VSPI for SD), e.g. with Waveshare ESP32 Driver Board
 //#define USE_HSPI_FOR_EPD
--- BertheVario/src/GlobalVar/CConfigFile.h
+++ BertheVario/src/GlobalVar/CConfigFile.h
@@ -4,7 +4,7 @@
 /// \brief
 ///
 /// \date creation     : 08/03/2024
-/// \date modification : 26/07/2024
+/// \date modification : 07/08/2024
 ///
 
 #ifndef _CCONFIGFILE_
@@ -35,14 +35,14 @@ public :
 
     void GetChar( int iVec , std::string & Name , std::string & Value ) const ;
 
-    float m_coef_filtre_alti_baro = 0.5 ;   ///< coefficient de filtrage [0,1[ , 0.99 tres fort filtrage
+    float m_coef_filtre_alti_baro = 0.4 ;   ///< coefficient de filtrage [0,1[ , 0.99 tres fort filtrage
     int   m_periode_integration_sec = 2 ;   ///< pour le calcul de la Vz
-    int   m_vitesse_igc_kmh = 18 ;          ///< vitesse de declenchement enregistrement
-    int   m_temps_igc_sec = 8 ;             ///< nombre de secondes de vitesse pour declenchement
+    int   m_vitesse_igc_kmh = 19 ;          ///< vitesse de declenchement enregistrement
+    int   m_temps_igc_sec = 10 ;             ///< nombre de secondes de vitesse pour declenchement
     float m_vz_igc_ms = 0.6 ;               ///< vz de vitesse pour declenchement
-    int   m_stab_gps = 20 ;                 ///< rayon dispersion gps pour stabilite
+    int   m_stab_gps = 30 ;                 ///< rayon dispersion gps pour stabilite
     float m_vz_seuil_max  = 4. ;            ///< seuils de beep
-    float m_vz_seuil_haut = 0.25 ;          ///< seuils de beep
+    float m_vz_seuil_haut = 0.2 ;          ///< seuils de beep
     float m_vz_seuil_bas = -2.5 ;           ///< seuils de beep
     int   m_dtu          = 2 ;              ///< diff heure TU
     int   m_AltiMargin   = 40 ;             ///< marge en altitude de zone TMA CTR en metres
