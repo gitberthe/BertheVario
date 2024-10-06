2d21e0b81148f13bbe47edde4a9f9d33374a93ee
SpinBerthe <spinberthe@gmail.com>
2024-10-06 16:48:51 +0200

    modification [temp_igc_sec] par defaut

--- BertheVario/src/GlobalVar/CConfigFile.h
+++ BertheVario/src/GlobalVar/CConfigFile.h
@@ -4,7 +4,7 @@
 /// \brief
 ///
 /// \date creation     : 08/03/2024
-/// \date modification : 20/09/2024
+/// \date modification : 06/10/2024
 ///
 
 #ifndef _CCONFIGFILE_
@@ -38,7 +38,7 @@ public :
     float m_coef_filtre_alti_baro = 0.4 ;   ///< coefficient de filtrage [0,1[ , 0.99 tres fort filtrage
     int   m_periode_integration_sec = 2 ;   ///< pour le calcul de la Vz
     int   m_vitesse_igc_kmh = 18 ;          ///< vitesse de declenchement enregistrement
-    int   m_temps_igc_sec = 5 ;             ///< nombre de secondes de vitesse pour declenchement
+    int   m_temps_igc_sec = 6 ;             ///< nombre de secondes de vitesse pour declenchement
     float m_vz_igc_ms = 0.7 ;               ///< vz de vitesse pour declenchement
     int   m_stab_gps_metre = 30 ;           ///< rayon dispersion gps pour stabilite
     int   m_stab_gps_sec = 10 ;             ///< taille pile en sec pour calcul rayon dispersion gps
