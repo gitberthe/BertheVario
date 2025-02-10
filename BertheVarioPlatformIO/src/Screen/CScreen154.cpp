e0cfb8f3c2eff44fa4d517c227492ab2149b1a94
SpinBerthe <spinberthe@gmail.com>
2025-02-10 02:47:31 +0100

    ihm tma proche

--- BertheVarioPlatformIO/src/Screen/CScreen154.cpp
+++ BertheVarioPlatformIO/src/Screen/CScreen154.cpp
@@ -4,7 +4,7 @@
 /// \brief
 ///
 /// \date creation     : 03/03/2024
-/// \date modification : 09/02/2025
+/// \date modification : 10/02/2025
 ///
 
 #include "../BertheVario.h"
@@ -489,9 +489,11 @@ else
         if ( AltFront > 500 )
             {
             display.setFont(&FreeMonoBold18pt7b);
-            display.print("___");
+            //            "500"
+            display.print("   ");
             display.setFont(&FreeMonoBold12pt7b);
-            display.print("__");
+            //            "A "
+            display.print("  ");
             }
         else
             {
@@ -505,7 +507,8 @@ else
         if ( DistFront > 999 )
             {
             display.setFont(&FreeMonoBold18pt7b);
-            display.print("_____");
+            //            "999SE"
+            display.print("     ");
             }
         else
             {
